7129f61ad1c779101f10611a0175f5e2c9a933ed
Frank <officialfrankromeo@gmail.com>
2022-07-12 17:10:16 -0400

    Added Library and Class

---
+++ SDK_Laboratoire1/LibrairieConversationNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,37 @@
+﻿namespace LibrairieConversationNote
+{
+    public static class ConversionAlphabetiqueToNote
+    {
+        public static string transform(string note)
+        {
+            switch (note)
+            {
+                case "A":
+                    return "LA";
+                    break;
+                case "B":
+                    return "SI";
+                    break;
+                case "C":
+                    return "DO";
+                    break;
+                case "D":
+                    return "RÉ";
+                    break;
+                case "E":
+                    return "MI";
+                    break;
+                case "F":
+                    return "FA";
+                    break;
+                case "G":
+                    return "SOL";
+                    break;
+                default:
+                    return "error";
+                    break;
+            }
+        }
+    }
+
+}
---
+++ SDK_Laboratoire1/SDK_Laboratoire1/Program.cs
@@ -0,0 +1,21 @@
+﻿using LibrairieConversationNote;
+
+while (true)
+{
+    Console.Write("Type de conversion à faire : ");
+    var input = Console.ReadLine();
+
+    //Si l'utilisateur tape note
+    if (input?.ToUpperInvariant() == "NOTE")
+    {
+        //On demande en boucle la conversion 
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+            string noteRetour = LibrairieConversationNote.ConversionAlphabetiqueToNote.transform(note.ToUpper());
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+}
